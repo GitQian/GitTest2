5f3e0ffc6cf5de1f5f86ec9b0d89d14d499f557a
qiansh <651564194@qq.com>
2016-08-17 15:01:18 +0800

    添加文件（上次提交忘记添加了）

---
+++ app/src/main/java/com/mesada/gittest/MyApplication.java
@@ -0,0 +1,13 @@
+package com.mesada.gittest;
+
+import android.app.Application;
+
+/**
+ * Created by user on 2016/8/17.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/mesada/gittest/activity/listview/ListViewActivity.java
@@ -0,0 +1,15 @@
+package com.mesada.gittest.activity.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mesada.gittest.R;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview);
+    }
+}
